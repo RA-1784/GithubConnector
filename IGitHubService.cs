400e7730ddf74755da72696e335145242778dea2
Rahul Anaparthi <rahulanaparthi005@gmail.com>
2026-03-30 11:46:07 +0530

    Add files via upload

---
+++ IGitHubService.cs
@@ -0,0 +1,11 @@
+﻿using GithubConnector.Models;
+namespace GithubConnector.Services
+{
+    public interface IGitHubService
+    {
+        Task<IEnumerable<Repo>> GetRepositoriesAsync(string username);
+        Task<IEnumerable<Issue>>GetIssuesAsync(string owner, string repo);
+
+        Task<bool> CreateIssueAsync(CreateIssueRequest request);
+    }
+}
