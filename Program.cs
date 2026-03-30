78390455d60df94214c5ac60398800e27e05f625
Rahul Anaparthi <rahulanaparthi005@gmail.com>
2026-03-30 11:51:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+
+using GithubConnector.Configuration;
+using GithubConnector.Services;
+
+namespace GithubConnector
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //config
+            builder.Services.Configure<GitHubSettings>(builder.Configuration.GetSection("GitHubSettings"));
+            //HttpClient
+            builder.Services.AddHttpClient<IGitHubService, GitHubService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
