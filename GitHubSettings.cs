c5744a42ac93177a54cf3deee300ef4b58d7975e
Rahul Anaparthi <rahulanaparthi005@gmail.com>
2026-03-30 11:41:51 +0530

    Add files via upload

---
+++ GitHubSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GithubConnector.Configuration
+{
+    public class GitHubSettings
+    {
+        public string ?BaseUrl { get; set; }
+        public string? Token {  get; set; }
+    }
+}
